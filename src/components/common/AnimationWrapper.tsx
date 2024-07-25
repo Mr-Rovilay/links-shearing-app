252c27cff9361d49b96c1d6ae286df077c559f05
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-07-25 15:39:42 +0100

    ani

--- src/components/common/AnimationWrapper.tsx
+++ src/components/common/AnimationWrapper.tsx
@@ -1,6 +1,14 @@
 import { AnimatePresence, motion } from "framer-motion";
+import React from "react";
 
-const AnimationWrapper = ({
+const AnimationWrapper: React.FC<{
+  children: React.ReactNode;
+  keyValue: string;
+  initial?: object;
+  animate?: object;
+  transition?: object;
+  className?: string;
+}> = ({
   children,
   keyValue,
   initial = { opacity: 0 },
